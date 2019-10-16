c5ad8679ae4f5b32529525f71999dd845805e81a
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-10-16 16:01:00 -0600

    Update sketch.js

--- p5js_connect_the_dots/sketch.js
+++ p5js_connect_the_dots/sketch.js
@@ -1,49 +1,50 @@
-var canvasWidth = 1400
-var canvasHeight = 700
-var circleRadius = 100
-var numRows = 5
-var numCols = 6
-var edgeSpace = 80
-var rowSpacing = (canvasWidth - (2 * edgeSpace)) / (numCols - 1)
-var colSpacing = (canvasHeight - (2 * edgeSpace)) / (numRows - 1)
+canvasWidth = 1400
+canvasHeight = 700
+circleRadius = 60
+numRows = 8
+numCols = 5
+edgeSpace = 80
+rowSpacing = (canvasWidth - (2 * edgeSpace)) / (numCols - 1)
+colSpacing = (canvasHeight - (2 * edgeSpace)) / (numRows - 1)
+x = 0
+y = 0
+counter = 0
 
-var counter = 0
 
 function setup() {
   createCanvas(canvasWidth,canvasHeight);
   angleMode(DEGREES);
-  noLoop();
+
   colorMode(HSB, 255, 255, 255);
-}
-
-function drawDelay(x,y) {
-	setInterval(drawSpot(x,y),1000);
-}
-
-function drawSpot(x, y) {
-  circle(edgeSpace+x*rowSpacing, edgeSpace+y*colSpacing, circleRadius);
+  stroke(0,0,250);
+  background(200,255,255);
+  fill (random(255),255,255)
+  //noLoop();
 }
 
 function draw() {
-	var hue = random(100);
-	background(200,0,120);
-	
-	for (j = 0; j <= numRows - 1; j++)
-	{
-		
-		for (i = 0; i <= numCols - 1; i++) //draws each row of circles
-		{
-			stroke(0,0,250);
-			fill(hue,255,255);
-			drawDelay(i,j); //trying to use setTimeout to individually display dots one-at-a-time
-			hue=hue+4;
-			if (hue >= 255)
-			{
-				hue = 0;
-			}
-		}
-	}
-
+	circle(edgeSpace + (x * rowSpacing), edgeSpace + (y*colSpacing), circleRadius);
 }
+
+function newCircle(){
+	counter++
+	var hue = random(255);
+	fill(hue,255,255);
+	x++
+  	if (x >= numCols)
+	  	{
+		  	x = 0
+		  	y++
+	  	}
+  	if (y >= numRows)
+	  	{
+	  		y = 0
+	  	}
+	 if (counter >= numRows * numCols - 1) 
+	 	noLoop()	
+  
+}
+
+setInterval(newCircle,50);
 
 
