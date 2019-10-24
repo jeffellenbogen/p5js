f33a5c7d4c631be4063d760c9f5140ca70680236
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-10-24 06:19:42 -0600

    clean up

--- circle_rect/sketch.js
+++ circle_rect/sketch.js
@@ -9,6 +9,12 @@ var numLevelsMax=20;
 var numLevelsSliderStart = 10;
 var tempLevels;
 
+var lineThickness;
+var lineThicknessMin=1;
+var lineThicknessMax=10;
+var lineThicknessStart=3;
+var templineThickness;
+
 var edgeBuffer = 50;
 var sidebarBufferWidth = 200;
 var mainBuffer;
@@ -24,16 +30,22 @@ function setup() {
   sideLen = windowHeight - edgeBuffer;
   numLevelsSlider = createSlider(numLevelsMin, numLevelsMax, numLevelsSliderStart);
   numLevels = numLevelsSliderStart;
+
+  lineThicknessSlider = createSlider(lineThicknessMin, lineThicknessMax, lineThicknessStart);
+  lineThickness = lineThicknessStart;
   noFill();
 }
 
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  mainBuffer = createGraphics(windowWidth - sidebarBufferWidth, windowHeight);
+  sidebarBuffer = createGraphics(sidebarBufferWidth, windowHeight);
+
   sideLen = windowHeight - edgeBuffer
   mainBuffer.fill(255);
   mainBuffer.stroke(255); 
-  mainBuffer.rect(0,0,windowWidth- sidebarBufferWidth , windowHeight)
+  mainBuffer.rect(0,0,windowWidth - sidebarBufferWidth , windowHeight)
 }
 
 function draw() {
@@ -43,10 +55,12 @@ function draw() {
   image(mainBuffer,sidebarBufferWidth,0);
   numLevelsSlider.position(35, 25);
   numLevels = numLevelsSlider.value();
+  lineThicknessSlider.position(35,150);
+  lineThickness = lineThicknessSlider.value();
 }
 
 function drawMainBuffer() {
-  mainBuffer.strokeWeight(4);
+
 	tempLevels = numLevelsSlider.value();
 	if (tempLevels != numLevels){
 		numLevels = tempLevels;
@@ -54,12 +68,21 @@ function drawMainBuffer() {
     mainBuffer.stroke(255); 
     mainBuffer.rect(0,0,sideLen, sideLen)
 	}
+  templineThickness = lineThicknessSlider.value();
+  if (templineThickness != lineThickness){
+    lineThickness = templineThickness;
+    mainBuffer.fill(255);
+    mainBuffer.stroke(255); 
+    mainBuffer.rect(0,0,sideLen, sideLen)
+  }
+
 	spacing = sideLen / numLevels/ 2;
 	for (var x = 1; x <= numLevels*2; x++)
 	{
 		increment = .5 * x * spacing;
 		if ((x % 2) == 1)
 		{
+      mainBuffer.strokeWeight(lineThickness);
       mainBuffer.noFill();
 			mainBuffer.stroke(0,200,255,150); // blue stroke
 			mainBuffer.rect(increment,increment,sideLen - (x*spacing), sideLen -(x*spacing));
@@ -71,14 +94,30 @@ function drawMainBuffer() {
 
 function drawSidebarBuffer(){
 
+  sidebarBuffer.fill(255);
+  sidebarBuffer.stroke(255); 
+  sidebarBuffer.rect(0,0,sidebarBufferWidth, windowHeight)
+
+  sidebarBuffer.fill(0);
   sidebarBuffer.stroke(0,200,255);
   sidebarBuffer.strokeWeight(3);
   sidebarBuffer.textSize(15);
-  sidebarBuffer.text(numLevelsMin + "   numLevels     " + numLevelsMax,35,60);
+  sidebarBuffer.text(numLevelsMin + "      numLevels      " + numLevelsMax,30,60);
 
   sidebarBuffer.stroke(255,0,150);
-  sidebarBuffer.strokeWeight(4);
+  sidebarBuffer.strokeWeight(2);
   sidebarBuffer.textSize(20);
-  sidebarBuffer.text("NumLevels = " + numLevels,35,120); 
+  sidebarBuffer.text("NumLevels = " + numLevels,35,90); 
+
+  sidebarBuffer.fill(0);
+  sidebarBuffer.stroke(0,200,255);
+  sidebarBuffer.strokeWeight(3);
+  sidebarBuffer.textSize(15);
+  sidebarBuffer.text(lineThicknessMin + "    strokeWeight     " + lineThicknessMax,30,190);
+
+  sidebarBuffer.stroke(255,0,150);
+  sidebarBuffer.strokeWeight(2);
+  sidebarBuffer.textSize(20);
+  sidebarBuffer.text("strokeWeight = " + lineThickness,35,220);   
 
 }
